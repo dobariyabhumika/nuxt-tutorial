8463485dffaf1dafb69bff350c6435a854bb9c01
Bhumika Dobariya <bhumika@improwised.com>
2019-03-18 17:35:58 +0530

    app and review page filter and pagination sorting added.

--- components/Apps.vue
+++ components/Apps.vue
@@ -1,10 +1,112 @@
 <template>
-  <b-container>
+  <div class="app-data">
     <div>
       <b-button variant="info" class="add-app-btn">Add App</b-button>
-      <b-table responsive striped hover :items="appData" />
+      <!-- <b-table responsive striped hover :items="appData" /> -->
+      <div>
+        <br>
+        <!-- User Interface controls -->
+        <b-row>
+          <b-col md="6" class="my-1">
+            <b-form-group label-cols-sm="3" label="Filter" class="mb-0">
+              <b-input-group>
+                <b-form-input v-model="filter" placeholder="Type to Search" />
+                <b-input-group-append>
+                  <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+                </b-input-group-append>
+              </b-input-group>
+            </b-form-group>
+          </b-col>
+
+          <b-col md="6" class="my-1">
+            <b-form-group label-cols-sm="3" label="Sort" class="mb-0">
+              <b-input-group>
+                <b-form-select v-model="sortBy" :options="sortOptions">
+                  <option slot="first" :value="null">-- none --</option>
+                </b-form-select>
+                <b-form-select :disabled="!sortBy" v-model="sortDesc" slot="append">
+                  <option :value="false">Asc</option> <option :value="true">Desc</option>
+                </b-form-select>
+              </b-input-group>
+            </b-form-group>
+          </b-col>
+
+          <b-col md="6" class="my-1">
+            <b-form-group label-cols-sm="3" label="Sort direction" class="mb-0">
+              <b-input-group>
+                <b-form-select v-model="sortDirection" slot="append">
+                  <option value="asc">Asc</option> <option value="desc">Desc</option>
+                  <option value="last">Last</option>
+                </b-form-select>
+              </b-input-group>
+            </b-form-group>
+          </b-col>
+
+          <b-col md="6" class="my-1">
+            <b-form-group label-cols-sm="3" label="Per page" class="mb-0">
+              <b-form-select :options="pageOptions" v-model="perPage" />
+            </b-form-group>
+          </b-col>
+        </b-row>
+
+        <!-- Main table element -->
+        <b-table
+          show-empty
+          stacked="md"
+          :items="appData"
+          :fields="fields"
+          :current-page="currentPage"
+          :per-page="perPage"
+          :filter="filter"
+          :sort-by.sync="sortBy"
+          :sort-desc.sync="sortDesc"
+          :sort-direction="sortDirection"
+          @filtered="onFiltered"
+        >
+          <template slot="name" slot-scope="row">
+            {{ row.value.first }} {{ row.value.last }}
+          </template>
+
+          <template slot="isActive" slot-scope="row">
+            {{ row.value ? 'Yes :)' : 'No :(' }}
+          </template>
+
+          <template slot="actions" slot-scope="row">
+            <b-button size="sm" @click="info(row.item, row.index, $event.target)" class="mr-1">
+              Info modal
+            </b-button>
+            <b-button size="sm" @click="row.toggleDetails">
+              {{ row.detailsShowing ? 'Hide' : 'Show' }} Details
+            </b-button>
+          </template>
+
+          <template slot="row-details" slot-scope="row">
+            <b-card>
+              <ul>
+                <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value }}</li>
+              </ul>
+            </b-card>
+          </template>
+        </b-table>
+
+        <b-row>
+          <b-col md="6" class="my-1">
+            <b-pagination
+              :total-rows="totalRows"
+              :per-page="perPage"
+              v-model="currentPage"
+              class="my-0"
+            />
+          </b-col>
+        </b-row>
+
+        <!-- Info modal -->
+        <b-modal id="modalInfo" @hide="resetModal" :title="modalInfo.title" ok-only>
+          <pre>{{ modalInfo.content }}</pre>
+        </b-modal>
+      </div>
     </div>
-  </b-container>
+  </div>
 </template>
 
 <script>
@@ -16,7 +118,48 @@
     },
     data() {
       return {
-        appData: jsondata
+        appData: jsondata,
+        fields: [
+          { key: 'app', label: 'App', sortable: true, sortDirection: 'desc' },
+          { key: 'category', label: 'Category', sortable: true, sortDirection: 'text-center' },
+          { key: 'rating', label: 'Rating', sortable: true, class: 'text-center' },
+          { key: 'installs', label: 'Installs', sortable: true, class: 'text-center' },
+        ],
+        currentPage: 1,
+        perPage: 5,
+        totalRows: jsondata.length,
+        pageOptions: [5, 10, 15],
+        sortBy: null,
+        sortDesc: false,
+        sortDirection: 'asc',
+        filter: null,
+        modalInfo: { title: '', content: '' }
+      }
+    },
+    computed: {
+      sortOptions() {
+        // Create an options list from our fields
+        return this.fields
+          .filter(f => f.sortable)
+          .map(f => {
+            return { text: f.label, value: f.key }
+          })
+      }
+    },
+    methods: {
+      info(item, index, button) {
+        this.modalInfo.title = `Row index: ${index}`
+        this.modalInfo.content = JSON.stringify(item, null, 2)
+        this.$root.$emit('bv::show::modal', 'modalInfo', button)
+      },
+      resetModal() {
+        this.modalInfo.title = ''
+        this.modalInfo.content = ''
+      },
+      onFiltered(filteredItems) {
+        // Trigger pagination to update the number of buttons/pages due to filtering
+        this.totalRows = filteredItems.length
+        this.currentPage = 1
       }
     }
   }
@@ -24,6 +167,10 @@
 
 <style>
 
+.app-data {
+  margin: 0 auto;
+  width:85%
+}
 .add-app-btn {
   margin: 15px 0;
   float: right
--- components/Reviews.vue
+++ components/Reviews.vue
@@ -1,22 +1,159 @@
 <template>
-  <b-container>
-    <div>
-      <b-button variant="info" class="add-app-btn">Add Review</b-button>
-      <b-table responsive striped hover :items="reviewData" />
-    </div>
-  </b-container>
+  <div class="reviews-data">
+    <br>
+    <!-- User Interface controls -->
+    <b-row>
+      <b-col md="6" class="my-1">
+        <b-form-group label-cols-sm="3" label="Filter" class="mb-0">
+          <b-input-group>
+            <b-form-input v-model="filter" placeholder="Type to Search" />
+            <b-input-group-append>
+              <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+            </b-input-group-append>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
+
+      <b-col md="6" class="my-1">
+        <b-form-group label-cols-sm="3" label="Sort" class="mb-0">
+          <b-input-group>
+            <b-form-select v-model="sortBy" :options="sortOptions">
+              <option slot="first" :value="null">-- none --</option>
+            </b-form-select>
+            <b-form-select :disabled="!sortBy" v-model="sortDesc" slot="append">
+              <option :value="false">Asc</option> <option :value="true">Desc</option>
+            </b-form-select>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
+
+      <b-col md="6" class="my-1">
+        <b-form-group label-cols-sm="3" label="Sort direction" class="mb-0">
+          <b-input-group>
+            <b-form-select v-model="sortDirection" slot="append">
+              <option value="asc">Asc</option> <option value="desc">Desc</option>
+              <option value="last">Last</option>
+            </b-form-select>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
+
+      <b-col md="6" class="my-1">
+        <b-form-group label-cols-sm="3" label="Per page" class="mb-0">
+          <b-form-select :options="pageOptions" v-model="perPage" />
+        </b-form-group>
+      </b-col>
+    </b-row>
+
+    <!-- Main table element -->
+    <b-table
+      show-empty
+      stacked="md"
+      :items="reviewData"
+      :fields="fields"
+      :current-page="currentPage"
+      :per-page="perPage"
+      :filter="filter"
+      :sort-by.sync="sortBy"
+      :sort-desc.sync="sortDesc"
+      :sort-direction="sortDirection"
+      @filtered="onFiltered"
+    >
+      <template slot="name" slot-scope="row">
+        {{ row.value.first }} {{ row.value.last }}
+      </template>
+
+      <template slot="isActive" slot-scope="row">
+        {{ row.value ? 'Yes :)' : 'No :(' }}
+      </template>
+
+      <template slot="actions" slot-scope="row">
+        <b-button size="sm" @click="info(row.item, row.index, $event.target)" class="mr-1">
+          Info modal
+        </b-button>
+        <b-button size="sm" @click="row.toggleDetails">
+          {{ row.detailsShowing ? 'Hide' : 'Show' }} Details
+        </b-button>
+      </template>
+
+      <template slot="row-details" slot-scope="row">
+        <b-card>
+          <ul>
+            <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value }}</li>
+          </ul>
+        </b-card>
+      </template>
+    </b-table>
+
+    <b-row>
+      <b-col md="6" class="my-1">
+        <b-pagination
+          :total-rows="totalRows"
+          :per-page="perPage"
+          v-model="currentPage"
+          class="my-0"
+        />
+      </b-col>
+    </b-row>
+
+    <!-- Info modal -->
+    <b-modal id="modalInfo" @hide="resetModal" :title="modalInfo.title" ok-only>
+      <pre>{{ modalInfo.content }}</pre>
+    </b-modal>
+  </div>
 </template>
 
 <script>
   import jsondata from '../static/json-data/reviews-data.json';
 
   export default {
-    name: 'Index',
+    name: 'Reviews',
     components: {
     },
     data() {
       return {
-        reviewData: jsondata
+        reviewData: jsondata,
+        fields: [
+          { key: 'translated_review', label: 'Translated Review', sortable: true, sortDirection: 'desc' },
+          { key: 'sentiment', label: 'Sentiment', sortable: true, sortDirection: 'text-center' },
+          { key: 'sentiment_polarity', label: 'Sentiment polarity', sortable: true, class: 'text-center' },
+          { key: 'sentiment_subjectivity', label: 'Sentiment Subjectivity', sortable: true, class: 'text-center' },
+        ],
+        currentPage: 1,
+        perPage: 5,
+        totalRows: jsondata.length,
+        pageOptions: [5, 10, 15],
+        sortBy: null,
+        sortDesc: false,
+        sortDirection: 'asc',
+        filter: null,
+        modalInfo: { title: '', content: '' }
+      }
+    },
+    computed: {
+      sortOptions() {
+        // Create an options list from our fields
+        return this.fields
+          .filter(f => f.sortable)
+          .map(f => {
+            return { text: f.label, value: f.key }
+          })
+      }
+    },
+    methods: {
+      info(item, index, button) {
+        this.modalInfo.title = `Row index: ${index}`
+        this.modalInfo.content = JSON.stringify(item, null, 2)
+        this.$root.$emit('bv::show::modal', 'modalInfo', button)
+      },
+      resetModal() {
+        this.modalInfo.title = ''
+        this.modalInfo.content = ''
+      },
+      onFiltered(filteredItems) {
+        // Trigger pagination to update the number of buttons/pages due to filtering
+        this.totalRows = filteredItems.length
+        this.currentPage = 1
       }
     }
   }
@@ -24,6 +161,10 @@
 
 <style>
 
+.reviews-data {
+  margin: 0 auto;
+  width:85%
+}
 .add-app-btn {
   margin: 15px 0;
   float: right
